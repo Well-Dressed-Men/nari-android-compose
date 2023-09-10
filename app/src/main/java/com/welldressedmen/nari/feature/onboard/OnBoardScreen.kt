2ac5000fd0e774c5ab1aa17a562d40dcfead2f55
Rudolf0328 <ck07160@hanyang.ac.kr>
2023-09-10 21:27:00 +0900

    tool bar

--- app/src/main/java/com/welldressedmen/nari/feature/onboard/OnBoardScreen.kt
+++ app/src/main/java/com/welldressedmen/nari/feature/onboard/OnBoardScreen.kt
@@ -1,24 +1,35 @@
 package com.welldressedmen.nari.feature.onboard
 
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.welldressedmen.nari.feature.main.Greeting
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun OnBoardScreen(name: String) {
-    Surface(
-        modifier = Modifier.fillMaxSize(), // = match parent
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Greeting(name) // Hello Android 라는 텍스트를 넣는다.
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("hello") },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+                    }
+                })
+        }
+    ) {it
+        Greeting(name = name)
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
