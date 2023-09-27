b57f241119afae2f032a9c64b127d3ca782bd004
kwakyongwoo <kkolbuyw@gmail.com>
2023-09-28 03:48:28 +0900

    NARI-5 ApiService function home(임시) 스켈레톤만 작성

--- app/src/main/java/com/welldressedmen/nari/data/remote/api/ApiService.kt
+++ app/src/main/java/com/welldressedmen/nari/data/remote/api/ApiService.kt
@@ -1,12 +1,16 @@
 package com.welldressedmen.nari.data.remote.api
 
-import com.welldressedmen.nari.data.remote.model.response.LoginResponse
+import com.welldressedmen.nari.data.remote.model.request.HomeRequestBody
 import com.welldressedmen.nari.data.remote.model.request.LoginRequestBody
-import retrofit2.Call
+import com.welldressedmen.nari.data.remote.model.response.HomeResponse
+import com.welldressedmen.nari.data.remote.model.response.LoginResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiService {
     @POST("oauth/jwt/google")
     suspend fun login(@Body requestBody: LoginRequestBody) : LoginResponse
+
+    @POST("HOME") // TODO: Change address
+    suspend fun home(@Body requestBody: HomeRequestBody) : HomeResponse // TODO: Change function name
 }
