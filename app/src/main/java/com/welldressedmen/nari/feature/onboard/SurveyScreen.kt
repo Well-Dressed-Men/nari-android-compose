ac914555b9ee8bf4ae1a12f666f278e76e9f8ccd
Rudolf0328 <ck07160@hanyang.ac.kr>
2023-10-10 16:28:00 +0900

    onBoardActivity 첫 페이지 완성

--- app/src/main/java/com/welldressedmen/nari/feature/onboard/SurveyScreen.kt
+++ app/src/main/java/com/welldressedmen/nari/feature/onboard/SurveyScreen.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
@@ -17,23 +19,49 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.welldressedmen.nari.R
 
 @Composable
 fun SurveyScreen(onClick: () -> Unit) {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Box(modifier = Modifier
-            .weight(1f)
-            .fillMaxWidth(), contentAlignment = Alignment.Center) {
-            Column(verticalArrangement = Arrangement.Center
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(horizontal = 16.dp)) {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth(), contentAlignment = Alignment.Center
+        ) {
+            Column(
+
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = "안녕! 시작하기 전에 간단한 질문 몇 가지만 할게")
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(horizontal = 16.dp),
+                    contentAlignment = Alignment.BottomCenter
+                ) {
+                    Text(
+                        text = "안녕! 시작하기 전에\n간단한 질문 몇 가지만 할게",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 25.sp,
+                        textAlign = TextAlign.Center
+                    )
+                }
                 Image(
                     painter = painterResource(id = R.drawable.chick),
                     contentDescription = "nari logo",
-                    modifier = Modifier.size(100.dp),
-                    Alignment.BottomCenter
+                    modifier = Modifier
+                        .size(200.dp)
+                        .weight(1f)
                 )
             }
         }
@@ -42,14 +70,21 @@ fun SurveyScreen(onClick: () -> Unit) {
             onClick = onClick,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp),
-            shape = CircleShape,
+                .padding(vertical = 24.dp),
+            contentPadding = PaddingValues(16.dp),
+            shape = RoundedCornerShape(16.dp),
             colors = ButtonDefaults.buttonColors(
                 backgroundColor = Color(0xFF18CD8C),
                 contentColor = Color.White
             )
         ) {
-            Text(text = "시작하기")
+            Text(
+                text = "시작하기", fontSize = 20.sp, style = TextStyle(
+                    platformStyle = PlatformTextStyle(
+                        includeFontPadding = false
+                    )
+                )
+            )
         }
     }
 }
